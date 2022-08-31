dcce2684ab57354ea52b701df275bb5acb2fed61
NikolasGumenyuk <nikos220599@gmail.com>
2022-08-31 17:02:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Task from "./components/Task";
+import TodoListSidebar from "./components/TodoListSidebar";
+
+const tasksArr = [
+  {
+    task_id: 1,
+    title: "Go to the gym",
+    description: "go go go",
+    done: false,
+    due_date: null,
+    list_id: 1,
+  },
+  {
+    task_id: 2,
+    title: "Go to the work",
+    description: "go go go",
+    done: false,
+    due_date: "2022-08-31T21:00:00.000Z",
+    list_id: 1,
+  },
+  {
+    task_id: 23,
+    title: "By something",
+    description: "by meat, by milk.",
+    done: false,
+    due_date: "2022-08-27T21:00:00.000Z",
+    list_id: 2,
+  },
+];
 
 function App() {
+  const [tasks, setTasks] = useState(tasksArr);
+  const [taskList, setTaskList] = useState([
+    {
+      tasklist_id: 1,
+      title: "study",
+    },
+    {
+      tasklist_id: 2,
+      title: "work",
+    },
+  ]);
+  let [selectedList, setSelectedList] = useState(tasks);
+
+  function toggleTask(id) {
+    setTasks(
+      tasks.map((task) => {
+        if (task.task_id === id) {
+          task.done = !task.done;
+        }
+        return task;
+      })
+    );
+  }
+
+  function deleteTask(id) {
+    setTasks(tasks.filter((task) => task.task_id !== id));
+  }
+
+  function selectTaskList(id) {
+    setSelectedList(tasksArr.filter((task) => task.list_id === id));
+    console.log(selectedList);
+  }
+
+  useEffect(() => {
+    setSelectedList(tasks);
+  }, [tasks]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoListSidebar
+        taskList={taskList}
+        onSelect={selectTaskList}
+        reset={() => setSelectedList(tasks)}
+      />
+      <ul className="tasks">
+        {selectedList.map((task) => (
+          <Task
+            task={task}
+            key={task.task_id}
+            onToggle={toggleTask}
+            deleteTask={deleteTask}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/components/Task/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import styles from "./index.module.css";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import Checkbox from "@mui/material/Checkbox";
+import CalendarTodayRoundedIcon from "@mui/icons-material/CalendarTodayRounded";
+
+function Task({ task, onToggle, deleteTask }) {
+  const [checked, setChecked] = useState(task.done);
+  const [formatedDate, setFormatedDate] = useState(new Date(task.due_date));
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  function formatDate(date) {
+    return `${date.getDate() < 10 ? "0" : ""}${date.getDate()}.${date.getMonth() < 10 ? "0" : ""}${date.getMonth()}.${date.getFullYear()}`;
+  }
+
+
+  return (
+    <li className={checked ? styles.done : styles.undone} key={task.task_id}>
+      {task.due_date ? <CalendarTodayRoundedIcon /> : ""}
+      {task.due_date ? formatDate(formatedDate) : ""}
+
+      <Checkbox
+        checked={checked}
+        onClick={() => onToggle(task.task_id)}
+        onChange={handleChange}
+        inputProps={{ "aria-label": "controlled" }}
+      />
+      <div>{task.title}</div>
+      <IconButton
+        aria-label="delete"
+        color="error"
+        onClick={() => deleteTask(task.task_id)}
+      >
+        <DeleteIcon />
+      </IconButton>
+    </li>
+  );
+}
+
+export default Task;
