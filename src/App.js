987c5022f83b70261cc15a2a32375ffaa9b6ec47
NikolasGumenyuk <nikos220599@gmail.com>
2022-12-01 21:36:55 +0200

    сщььше

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import TodoListSidebar from "./components/TodoListSidebar/TodoListSidebar";
-import axios from "axios";
 import AppRouter from "./AppRouter";
-import {getAllTasksList, getDashboard} from "./services/RequestApi"
-import {loadDashboard} from "./store/dashboard/actions"
+import { getAllTasksList, getDashboard } from "./services/RequestApi";
 import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   const [taskList, setTaskList] = useState([]);
   const dispatch = useDispatch();
   const dashboard = useSelector((state) => state.dashboard);
-  
-  // dashboard.lists.map((list) => console.log(list.undone));
-
 
   useEffect(() => {
-    getDashboard().then((data) => dispatch({type: "LOAD_DASHBOARD", payload: data}));
+    getDashboard().then((data) =>
+      dispatch({ type: "LOAD_DASHBOARD", payload: data })
+    );
 
     getAllTasksList().then(setTaskList);
   }, []);
 
+      СІФАІАІА
   return (
     <div>
-      <TodoListSidebar taskList={taskList} dashboard={dashboard}/>
+      <TodoListSidebar taskList={taskList} dashboard={dashboard} />
 
       <AppRouter />
     </div>
